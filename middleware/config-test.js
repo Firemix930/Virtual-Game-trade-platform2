9a237ed5e522b0239bf32868ed093426768c0ad9
unknown <240921128@163.com>
2022-05-07 20:22:39 +0800

    Zshop

---
+++ middleware/config-test.js
@@ -0,0 +1,11 @@
+var config = {
+    dialect: 'mysql',
+    database: 'zshop',
+    username: 'root',
+    password: 'zhx1234',
+    host: 'localhost',
+    port: 3306,
+    rootpath: '/zshop'
+};
+
+module.exports = config;
